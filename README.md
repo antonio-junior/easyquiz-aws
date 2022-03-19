b916228df444e9be9480b9fcff8a167194ca094d
Antonio Junior <acsjunior1@gmail.com>
2022-03-19 17:18:30 -0300

    first commit

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Welcome to your CDK TypeScript project
+# easyquiz-aws
 
-This is a blank project for TypeScript development with CDK.
+Infrastructure As Code (IaC) designed for Easyquiz Application using AWS Cloud Development Kit (AWS CDK)
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+The `cdk.json` file tells the CDK Toolkit how to execute yhe app.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
--- lib/easyquiz-aws-stack.ts
+++ lib/easyquiz-aws-stack.ts
@@ -1,16 +1,75 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as dotenv from 'dotenv'
+dotenv.config()
 export class EasyquizAwsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Read from .env file
+    const { 
+      PORT, SECRET_KEY, DB_USER, DB_PWD, DB_HOST, 
+      DB_PORT, DB_NAME, DB_DIALECT, HOST, EMAIL, 
+      MAIL_HOST, MAIL_USER, MAIL_PASS, MAIL_PORT,
+      VPC_DB, SG_DB, ENABLE_PLAYGROUND
+    } = process.env;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EasyquizAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Database VPC
+    const vpc = ec2.Vpc.fromLookup(this, "EasyQuizVpc", {
+      region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION ,
+      vpcId: VPC_DB
+    });
+
+    const cluster = new ecs.Cluster(this, "EasyQuizCluster", {
+      vpc: vpc,
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    const lb = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "EasyQuizFargateService", {
+      cluster: cluster,
+      desiredCount: 2,
+      taskImageOptions: { 
+        image: ecs.ContainerImage.fromRegistry("antoniocsjunior/easyquiz-graphql"),
+        containerName: "easyquiz-graphql",
+        environment: {
+          PORT: PORT || '',
+          SECRET_KEY: SECRET_KEY || '',
+          DB_USER: DB_USER || '',
+          DB_PWD: DB_PWD || '',
+          DB_HOST: DB_HOST || '',
+          DB_PORT: DB_PORT || '',
+          DB_NAME: DB_NAME || '',
+          DB_DIALECT: DB_DIALECT || '',
+          HOST: HOST || '',
+          EMAIL: EMAIL || '',
+          MAIL_HOST: MAIL_HOST || '',
+          MAIL_USER: MAIL_USER || '',
+          MAIL_PASS: MAIL_PASS || '',
+          MAIL_PORT: MAIL_PORT || '',
+          ENABLE_PLAYGROUND: ENABLE_PLAYGROUND || ''
+        },
+        containerPort: parseInt(PORT || ''),
+      },
+      assignPublicIp: true,
+      publicLoadBalancer: true,
+    });
+
+    const importedSecurityGroup = ec2.SecurityGroup.fromSecurityGroupId(
+      this,
+      'database-security-group',
+      SG_DB as string,
+      { allowAllOutbound: false }
+    );
+
+    lb.service.connections.allowTo(importedSecurityGroup,  ec2.Port.tcp(parseInt(DB_PORT || '')))
+
+    lb.targetGroup.configureHealthCheck({
+      path: "/check",
+      enabled: true,
+      healthyHttpCodes: '200'
+    });
   }
 }
